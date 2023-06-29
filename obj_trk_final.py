0381e0a127795bdbef5e4b658914c4fccd8dadde
kjeshwanth <103258143+kjeshwanth@users.noreply.github.com>
2023-06-29 22:26:46 +0530

    Add files via upload

--- obj_trk_final.py
+++ obj_trk_final.py
@@ -47,16 +47,6 @@ def drawBox(img,bbox):
 
 
 
-
-def drawBox(img, bbox):
-    x, y, w, h = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
-
-    cv2.rectangle(img,(x,y),((x+w),(y+h)),(255,0,255),3,1)
-
-    cv2.putText(img,"Tracking",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,255,0),2)
-    pass
-
-
 while True:
     check, img = video.read()   
     success, bbox = tracker.update(img)
